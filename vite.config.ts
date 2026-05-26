46fe3aa36ce4baf256ba9884987db655700053d8
pape abdoulaye samb <sambpalayesaamb103@gmail.com>
2026-05-26 21:39:55 +0000

    Configurer pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
+    ssr: false,
+  },
+  vite: {
+    base: "/site-matt-groupe/",
   },
 });
